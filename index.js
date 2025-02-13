553019bf1983f852dcf370fbb25ccd7019714688
AdniSosa <adni.sori@gmail.com>
2025-02-13 15:06:59 +0100

    Servidor, db terminados. Product.js en proceso

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-//npm init -y (package json)
-
 //Crear un Servidor Express
+const express = require('express');
+const app = express();
+const dbConnection = require('./config/db')
+require('dotenv').config();
+
+dbConnection();
+
+app.listen(process.env.PORT, () => console.log(`Server listening on http://localhost:${process.env.PORT}`));
