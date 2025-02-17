23fcae94818f0f04a032c242643b185bac2bc01c
MarianaLGM <mariana_lobeto@hotmail.com>
2025-02-17 17:33:43 +0100

    cambios mariana

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,198 @@
 const Product = require("../models/Product.js");
 const User = require("../models/User.js");
 
+
+////////////////////////////////Funciones para generar el HTML/////////////////////////////////////////
+
+// VISTA DASHBOARD getProductCards: Genera el html de los productos. Recibe un array de productos y devuelve el html de las tarjetas de los productos.
+function getProductCardsDashboard(products) {
+    let html = '';
+
+    if(products.length > 0) { 
+        for (let product of products) {
+            html += `
+                <div class="product-card">
+                    <img src="${product.Imagen}" alt="${product.Nombre}">
+                    <h2>${product.Nombre}</h2>
+                    <p>${product.Descripción}</p>
+                    <p>${product.Precio}€</p>
+                    <a href="/products/${product._id}">Ver detalle</a>
+                    <a href="/products/${product._id}/edit">Editar</a>
+                    <a href="/products/${product._id}/delete">Eliminar</a>
+                </div>`
+        }
+        return html;
+    } else {
+        html = `
+            <div class="product-card">
+                <img src="${products.Imagen}" alt="${products.Nombre}">
+                <h2>${products.Nombre}</h2>
+                <p>${products.Descripción}</p>
+                <p>${products.Precio}€</p>
+                <a href="/products/${products._id}">Ver detalle</a>
+                <a href="/products/${products._id}/edit">Editar</a>
+                <a href="/products/${products._id}/delete">Eliminar</a>
+            </div>
+        `;
+        
+        return html;
+    }
+}
+
+//VISTA GENERAL getProductCardsClient Genera el html de los productos. Recibe un array de productos y devuelve el html de las tarjetas de los productos
+function getProductCards(products) {
+    let html = '';
+
+    if(products.length > 0) { 
+        for (let product of products) {
+            html += `
+                <div class="product-card">
+                    <img src="${product.Imagen}" alt="${product.Nombre}">
+                    <h2>${product.Nombre}</h2>
+                    <p>${product.Descripción}</p>
+                    <p>${product.Precio}€</p>
+                    <a href="/products/${products._id}">Ver detalle</a>
+                </div>`
+        }
+        return html;
+    } else {
+        html = `
+            <div class="product-card">
+                <img src="${products.Imagen}" alt="${products.Nombre}">
+                <h2>${products.Nombre}</h2>
+                <p>${products.Descripción}</p>
+                <p>${products.Precio}€</p>
+                <a href="/products/${products._id}">Ver detalle</a>
+            </div>
+        `;
+        
+        return html;
+    }
+}
+
+//baseHtml: html común a todas las páginas. Puede contener elementos como la importación de estilos, etc.
+const baseHtml =
+`
+    <!DOCTYPE html>
+    <html lang="es">
+        <head>
+        <meta charset="UTF-8">
+        <title>Tienda de ropa online</title>
+        <link rel="stylesheet" href="./public/style.css">
+        </head>
+`;
+
+const successfullyUpdated=
+`
+    <h2>Product successfully updated</h2>
+`;
+
+
+//getNavBar: Genera la barra de navegación con las categorías. También generará un enlace para subir un nuevo producto.
+const getNavBar= 
+`
+    <header class="header"> 
+    <nav>
+        <div class="container">
+            <ul class="nav1"> 
+                <li><a href="/products"class="navigation">Productos</a></li>
+                <li><a href="/camisetas"class="navigation">Camisetas</a></li>
+                <li><a href="/pantalones"class="navigation">Pantalones</a></li>
+                <li><a href="/zapatos"class="navigation">Zapatos</a></li>
+                <li><a href="/accesorios"class="navigation">Accesorios</a></li>
+                <li><a href="/login"class="navigation">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+    </header>
+    `;
+
+
+//VISTA DASHBOARD formCreateProduct, formulario para crear producto:
+const formCreateProduct = 
+`
+    <body>
+        <form action='/dashboard'>
+            <label for="productImg">Select files:</label>
+            <input type="file" id="productImg" name="productImg" required>
+                    
+            <label for='productName'>Nombre del producto: </label>
+            <input id='productName' type='text' name='productName' required>
+                    
+            <label for='productDescription'>Descripción del producto: </label>
+            <input id='productDescription' type='text' name='productDescription' required>
+                    
+            <label for='productCategory'>Categoría del producto: </label>
+            <select id="productCategory" name="productCategory" required>
+                <option value="Camisetas">Camisetas</option>
+                <option value="Pantalones">Pantalones</option>
+                <option value="Zapatos">Zapatos</option>
+                <option value="Accesorios">Accesorios</option>
+            </select>
+                    
+            <label for='productSize'>Talla del producto: </label>
+            <select id="productSize" name="productSize" required>
+                <option value="XS">XS</option>
+                <option value="S">S</option>
+                <option value="M">M</option>
+                <option value="L">L</option>
+                <option value="XL">XL</option>
+            </select>
+
+            <label for='productPrice'>Precio del producto: </label>
+            <input id='productPrice' type='number' name='productPrice' min='0' required>
+
+            <button id=newProductBtn type='submit'>Enviar</button>
+
+            </form>
+        </body>
+    `;
+
+
+//VISTA DASHBOARD formEditProduct, formulario para editar producto:
+const formEditProduct = (product) => {
+    return `
+    <body>
+        <form action='/dashboard/:productId' method='post'>
+
+                <label for="productImg">Select files:</label>
+                <input type="file" id="productImg" name="productImg"><br>
+                
+                <label for='productName'>Nombre del producto: </label>
+                <input id='productName' type='text' name='productName' value='${product.Nombre}'><br>
+                
+                <label for='productDescription'>Descripción del producto: </label>
+                <textarea id='productDescription' type='text' name='productDescription' required value='${product.Descripción}'></textarea><br>
+                
+                <label for='productCategory'>Categoría del producto: </label>
+                <select id="productCategory" name="productCategory">
+                    <option value="camisetas">Camisetas</option>
+                    <option value="pantalones">Pantalones</option>
+                    <option value="zapatos">Zapatos</option>
+                    <option value="accesorios">Accesorios</option>
+                </select><br>
+                
+                <label for='productSize'>Talla del producto: </label>
+                <select id="productSize" name="productSize">
+                    <option value="XS">XS</option>
+                    <option value="S">S</option>
+                    <option value="M">M</option>
+                    <option value="L">L</option>
+                    <option value="XL">XL</option>
+                </select><br>
+
+                <label for='productPrice'>Precio del producto: </label>
+                <input id='productPrice' type='number' name='productPrice' min='0' value='${product.Precio}'><br>
+
+                <button id=newProductBtn type='submit'>Enviar</button>
+
+            </form>
+        </body>
+    `;
+};
+
+
+
 /**************************************VISTA GENERAL***************************************/
 
 //showProducts: Devuelve la vista con todos los productos
@@ -179,195 +371,6 @@ const deleteProduct = async (req, res) => {
             .json({ message: "There was a problem trying to delete a product" });
     }
 };
-////////////////////////////////Funciones auxiliares para generar el HTML/////////////////////////////////////////
-
-// VISTA DASHBOARD getProductCards: Genera el html de los productos. Recibe un array de productos y devuelve el html de las tarjetas de los productos.
-function getProductCardsDashboard(products) {
-    let html = '';
-
-    if(products.length > 0) { 
-        for (let product of products) {
-            html += `
-                <div class="product-card">
-                    <img src="${product.Imagen}" alt="${product.Nombre}">
-                    <h2>${product.Nombre}</h2>
-                    <p>${product.Descripción}</p>
-                    <p>${product.Precio}€</p>
-                    <a href="/products/${product._id}">Ver detalle</a>
-                    <a href="/products/${product._id}/edit">Editar</a>
-                    <a href="/products/${product._id}/delete">Eliminar</a>
-                </div>`
-        }
-        return html;
-    } else {
-        html = `
-            <div class="product-card">
-                <img src="${products.Imagen}" alt="${products.Nombre}">
-                <h2>${products.Nombre}</h2>
-                <p>${products.Descripción}</p>
-                <p>${products.Precio}€</p>
-                <a href="/products/${products._id}">Ver detalle</a>
-                <a href="/products/${products._id}/edit">Editar</a>
-                <a href="/products/${products._id}/delete">Eliminar</a>
-            </div>
-        `;
-        
-        return html;
-    }
-}
-
-//VISTA GENERAL getProductCardsClient Genera el html de los productos. Recibe un array de productos y devuelve el html de las tarjetas de los productos
-function getProductCards(products) {
-    let html = '';
-
-    if(products.length > 0) { 
-        for (let product of products) {
-            html += `
-                <div class="product-card">
-                    <img src="${product.Imagen}" alt="${product.Nombre}">
-                    <h2>${product.Nombre}</h2>
-                    <p>${product.Descripción}</p>
-                    <p>${product.Precio}€</p>
-                    <a href="/products/${products._id}">Ver detalle</a>
-                </div>`
-        }
-        return html;
-    } else {
-        html = `
-            <div class="product-card">
-                <img src="${products.Imagen}" alt="${products.Nombre}">
-                <h2>${products.Nombre}</h2>
-                <p>${products.Descripción}</p>
-                <p>${products.Precio}€</p>
-                <a href="/products/${products._id}">Ver detalle</a>
-            </div>
-        `;
-        
-        return html;
-    }
-}
-
-//baseHtml: html común a todas las páginas. Puede contener elementos como la importación de estilos, etc.
-const baseHtml =
-`
-    <!DOCTYPE html>
-    <html lang="es">
-        <head>
-        <meta charset="UTF-8">
-        <title>Tienda de ropa online</title>
-        <link rel="stylesheet" href="./public/style.css">
-        </head>
-`;
-
-const successfullyUpdated=
-`
-    <h2>Product successfully updated</h2>
-`;
-
-
-//getNavBar: Genera la barra de navegación con las categorías. También generará un enlace para subir un nuevo producto.
-const getNavBar= 
-`
-    <header class="header"> 
-    <nav>
-        <div class="container">
-            <ul class="nav1"> 
-                <li><a href="/products"class="navigation">Productos</a></li>
-                <li><a href="/camisetas"class="navigation">Camisetas</a></li>
-                <li><a href="/pantalones"class="navigation">Pantalones</a></li>
-                <li><a href="/zapatos"class="navigation">Zapatos</a></li>
-                <li><a href="/accesorios"class="navigation">Accesorios</a></li>
-                <li><a href="/login"class="navigation">Login</a></li>
-            </ul>
-        </div>
-    </nav>
-    </header>
-    `;
-
-
-//VISTA DASHBOARD formCreateProduct, formulario para crear producto:
-const formCreateProduct = 
-`
-    <body>
-        <form action='/dashboard'>
-            <label for="productImg">Select files:</label>
-            <input type="file" id="productImg" name="productImg" required>
-                    
-            <label for='productName'>Nombre del producto: </label>
-            <input id='productName' type='text' name='productName' required>
-                    
-            <label for='productDescription'>Descripción del producto: </label>
-            <input id='productDescription' type='text' name='productDescription' required>
-                    
-            <label for='productCategory'>Categoría del producto: </label>
-            <select id="productCategory" name="productCategory" required>
-                <option value="Camisetas">Camisetas</option>
-                <option value="Pantalones">Pantalones</option>
-                <option value="Zapatos">Zapatos</option>
-                <option value="Accesorios">Accesorios</option>
-            </select>
-                    
-            <label for='productSize'>Talla del producto: </label>
-            <select id="productSize" name="productSize" required>
-                <option value="XS">XS</option>
-                <option value="S">S</option>
-                <option value="M">M</option>
-                <option value="L">L</option>
-                <option value="XL">XL</option>
-            </select>
-
-            <label for='productPrice'>Precio del producto: </label>
-            <input id='productPrice' type='number' name='productPrice' min='0' required>
-
-            <button id=newProductBtn type='submit'>Enviar</button>
-
-            </form>
-        </body>
-    `;
-
-
-//VISTA DASHBOARD formEditProduct, formulario para editar producto:
-const formEditProduct = (product) => {
-    return `
-    <body>
-        <form action='/dashboard/:productId' method='post'>
-
-                <label for="productImg">Select files:</label>
-                <input type="file" id="productImg" name="productImg"><br>
-                
-                <label for='productName'>Nombre del producto: </label>
-                <input id='productName' type='text' name='productName' value='${product.Nombre}'><br>
-                
-                <label for='productDescription'>Descripción del producto: </label>
-                <textarea id='productDescription' type='text' name='productDescription' required value='${product.Descripción}'></textarea><br>
-                
-                <label for='productCategory'>Categoría del producto: </label>
-                <select id="productCategory" name="productCategory">
-                    <option value="camisetas">Camisetas</option>
-                    <option value="pantalones">Pantalones</option>
-                    <option value="zapatos">Zapatos</option>
-                    <option value="accesorios">Accesorios</option>
-                </select><br>
-                
-                <label for='productSize'>Talla del producto: </label>
-                <select id="productSize" name="productSize">
-                    <option value="xs">XS</option>
-                    <option value="s">S</option>
-                    <option value="m">M</option>
-                    <option value="l">L</option>
-                    <option value="xl">XL</option>
-                </select><br>
-
-                <label for='productPrice'>Precio del producto: </label>
-                <input id='productPrice' type='number' name='productPrice' min='0' value='${product.Precio}'><br>
-
-                <button id=newProductBtn type='submit'>Enviar</button>
-
-            </form>
-        </body>
-    `;
-};
-
 
 
 
